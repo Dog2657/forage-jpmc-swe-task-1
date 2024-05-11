4441b2f289cf025fc560eb7e141e11b0117605f1
Dog2657 <reeceya2006@gmail.com>
2024-05-11 17:07:03 +0100

    Added tests

--- client_test.py
+++ client_test.py
@@ -1,23 +1,43 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
+  def test_get_ratio(self):
+    tests = [
+      {"exp": 10, "arg1": 100, "arg2": 10},
+      {"exp": 20, "arg1": 100, "arg2": 5},
+      {"exp": 100, "arg1": 200, "arg2": 2},
+      {"exp": 100, "arg1": 1000, "arg2": 10},
+    ]
+    
+    for test in tests:
+      self.assertEqual(test["exp"], getRatio(test["arg1"], test["arg2"]))
+
   def test_getDataPoint_calculatePrice(self):
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    
+    for quote in quotes:
+      top_price: float = quote["top_bid"]["price"]
+      top_ask: float = quote["top_ask"]["price"]
+
+      self.assertEqual( getDataPoint(quote), (quote.get("stock"), top_price, top_ask, (top_price + top_ask) / 2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
 
+    for quote in quotes:
+      top_price: float = quote["top_bid"]["price"]
+      top_ask: float = quote["top_ask"]["price"]
 
-  """ ------------ Add more unit tests ------------ """
+      self.assertEqual( getDataPoint(quote), (quote.get("stock"), top_price, top_ask, (top_price + top_ask) / 2))
+
+  
 
 
 
